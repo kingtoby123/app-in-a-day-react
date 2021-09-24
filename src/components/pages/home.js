e304e2d7f76bab7c1ec2d8681e1fbcff6c58b5d6
Courtney Morris <shull.courtney@gmail.com>
2021-09-24 11:12:25 -0600

    Added stuff

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,5 +1,26 @@
 import React, { useState } from "react";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="home-wrapper">
+      <div className="featured-content-wrapper">
+        <div className="featured-photo-wrapper">
+          <img src="https://i.postimg.cc/FztVMTHL/Other-Pic-Courtney.jpg" />
+          <div className="name-wrapper">Courtney</div>
+        </div>
+        <div className="featured-photo-wrapper">
+          <img src="https://i.postimg.cc/XvNpHdKk/Full-Outfit-Patrick.jpg" />
+          <div className="name-wrapper">Patrick</div>
+        </div>
+        <div className="featured-photo-wrapper">
+          <img src="https://i.postimg.cc/CM8L4cTV/Full-Outfit-Seth.jpg" />
+          <div className="name-wrapper">Seth</div>
+        </div>
+        <div className="featured-photo-wrapper">
+          <img src="https://i.postimg.cc/Y052fm8k/Full-Outfit-Tobye.jpg" />
+          <div className="name-wrapper">Toby</div>
+        </div>
+      </div>
+    </div>
+  );
 }
